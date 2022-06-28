37c357aeb17641beb78c9165d08d35d9c234fd7a
marcocalligaris <marcocally@gmail.com>
2022-06-28 07:44:06 +0200

    Images gallery

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 // * MILESTONE 2
 // * Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for. Possiamo concatenare una stringa con un template literal oppure utilizzare gli altri metodi di manipolazione del DOM che abbiamo visto insieme. Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente attraverso JavaScript.
 
+
+// * Preparo l'array con le immagini
+
 const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg",];
-let activeIndex = 0;
 
 // * Mi prendo dal DOM gli elementi che mi servono
 
 const gallery = document.getElementById('gallery');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+// * Creo variabile per l'immagine attiva
+
+let activeIndex = 0;
+images[activeIndex]classList.add('active');
 
 let image = '';
 
 for (let i = 0; i < images.length; i++) {
     image = `<img src="${images[i]}">`
+    image[activeIndex].classList.add('active');
 }
 
 gallery.innerHTML = image;
+
+nextButton.addEventListener('click', function() {
+    
+})
 
 
 
