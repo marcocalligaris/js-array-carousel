beb5ad8b6885503fe1f6c86b4a016dfc412fba69
marcocalligaris <marcocally@gmail.com>
2022-06-30 08:16:02 +0200

    Carousel

--- js/script.js
+++ js/script.js
@@ -3,32 +3,57 @@
 
 
 // * Preparo l'array con le immagini
+const sources = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
-const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg",];
 
 // * Mi prendo dal DOM gli elementi che mi servono
-
-const gallery = document.getElementById('gallery');
+const images = document.querySelectorAll('#carousel img');
+const gallery = document.querySelector('#carousel .gallery');
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 
+// // * Aggiunta della gallery di immagini
+// let image = '';
+// for (let i = 0; i < sources.length; i++) {
+//     image += `<img src="${sources[i]}" alt="landscape" />`;
+// }
+
+// gallery.innerHTML = image;
+
 // * Creo variabile per l'immagine attiva
-
 let activeIndex = 0;
-images[activeIndex]classList.add('active');
 
-let image = '';
 
-for (let i = 0; i < images.length; i++) {
-    image = `<img src="${images[i]}">`
-    image[activeIndex].classList.add('active');
-}
+// * Classe active per la prima immagine
+images[activeIndex].classList.add('active');
 
-gallery.innerHTML = image;
 
-nextButton.addEventListener('click', function() {
-    
+
+// * Next button
+nextButton.addEventListener('click', function(){
+    images[activeIndex].classList.remove('active');
+    activeIndex++;
+    if (activeIndex === sources.length) {
+        activeIndex = 0;
+    }
+    images[activeIndex].classList.add('active');
 })
+
+
+// * Prev button
+prevButton.addEventListener('click', function() {
+    images[activeIndex].classList.remove('active');
+    activeIndex--;
+    images[activeIndex].classList.add('active');
+    if(activeIndex < 0) {
+        activeIndex = sources.length - 1;
+    }
+})
+
+
+
+
+
 
 
 
